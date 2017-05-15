9e5d0eb772932b3c2f970f876507441bcf41aba4
b.wisman155 <b.wisman155@gmail.com>
2017-05-15 21:20:17 +0200

    Wrote test

--- tests/test_api.rs
+++ tests/test_api.rs
@@ -1,7 +1,15 @@
+extern crate pastebin_rust_api;
+
 #[cfg(test)]
 mod tests {
+    use pastebin_rust_api::paster::Paster;
+    use pastebin_rust_api::paster::access::Access;
+
     #[test]
-    fn it_works() {
-        println!("IT WORKS");
+    fn test_post() {
+        let parser = Paster::new(Some("e27fc5e3a33b4a5e7b509e9d06723fcc".to_owned()));
+        let response = parser.paste("<html></html>", &Access::UNLISTED, "TestHtml", "1M", "html", "");
+        assert!(response.is_ok());
+        println!("url: {}", response.ok().unwrap().url);
     }
 }
