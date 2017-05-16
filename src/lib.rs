4f2f72c22ec31e82a71bd0dc2130fe6418c38d79
DerUberCatman <b.wisman155@gmail.com>
2017-05-16 15:22:59 +0200

    updated documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,38 +1,43 @@
+
+//! # Pastebin Rust Api
+//!
+//! Wrapper for the [Pastebin Api](https://pastebin.com/api)
+//!
+//! ## Getting started
+//!
+//! Simple usage of `Paster`:
+//!
+//! ```
+//! // I recommend to put your dev key into an environment variable called `PASTEBIN_DEVELOPER_TOKEN`.
+//! let parser = Paster::new(Some("<YOUR DEV KEY>".to_owned()));
+//! let response = parser.paste("<html></html>",
+//!                             &Access::Private,
+//!                             "TestHtml",
+//!                             &Expiration::TenMinutes,
+//!                             &Format::HTML5,
+//!                             None);
+//! if response.is_ok() {
+//!     if let Some(paste) = response.ok() {
+//!         // If everything is OK, you can get the url to your code here.
+//!         println!("{}", paste.url);
+//!     }
+//! }
+//!
+//! ```
+//!
+
 extern crate reqwest;
 
 pub use self::paster::Paster;
-pub use self::objects::PastebinMessage;
 pub use self::paster::expiration::Expiration;
 pub use self::paster::access::Access;
 pub use self::paster::format::Format;
 pub use self::error::Error;
 
 mod paster;
-pub mod objects;
+mod objects;
 mod error;
 
-/// # Examples
-///
-/// Simple usage of `Paster`:
-///
-/// ```
-/// /// I recommend to put your dev key into an environment variable called `PASTEBIN_DEVELOPER_TOKEN`.
-/// let parser = Paster::new(Some("<YOUR DEV KEY>".to_owned()));
-/// let response = parser.paste("<html></html>",
-///                             &Access::Private,
-///                             "TestHtml",
-///                             &Expiration::TenMinutes,
-///                             &Format::HTML5,
-///                             None);
-/// if response.is_ok() {
-///     if let Some(paste) = response.ok() {
-///         /// If everything is OK, you can get the url to your code here.
-///         println!("{}", paste.url);
-///     }
-/// }
-///
-/// ```
-///
 
 /// method for constructing the api url for pastebin.
 fn construct_api_url(path: &[&str]) -> String {
