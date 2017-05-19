1edcfb9dd36ac03fde7bece79fbd3ce675da573b
DerUberCatman <b.wisman155@gmail.com>
2017-05-19 11:21:23 +0200

    added login_post_test

--- tests/test_api.rs
+++ tests/test_api.rs
@@ -24,8 +24,27 @@ mod tests {
         let paster = Paster::new(None);
         let response = paster.login(None, None);
         assert!(response.is_ok());
-        if let Some(message) = response.ok() {
-            println!("user_key: {}", message.content);
+        if let Some(user_key) = response.ok() {
+            println!("user_key: {}", user_key.content);
+        }
+    }
+
+    #[test]
+    fn test_login_post() {
+        let paster = Paster::new(None);
+        let user_key_response = paster.login(None, None);
+        assert!(user_key_response.is_ok());
+        if let Some(user_key) = user_key_response.ok() {
+            let url_response = paster.paste("<html></html>",
+                                        Some(&Access::Private),
+                                        Some("TestHtml"),
+                                        Some(&Expiration::TenMinutes),
+                                        Some(&Format::HTML5),
+                                        Some(&user_key.content));
+            assert!(url_response.is_ok());
+            if let Some(message) = url_response.ok() {
+                println!("URL: {}", message.content);
+            }
         }
     }
 }
